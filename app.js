3bc098406952b177040fd1e38429689437fb4597
aniket-bhadra <aniketbhadra98320@gmail.com>
2024-02-21 17:42:20 +0530

    created play  music page

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("hello");
+let mySong = document.getElementById("mySong");
+let icon = document.getElementById("icon");
+let text = document.getElementById("text");
 
-
+icon.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    icon.src = "assets/pause2.png";
+    text.innerText = "Freeze the Beats";
+  } else {
+    mySong.pause();
+    icon.src = "assets/play2.png";
+    text.innerText = "Hit Play Now";
+  }
+};
