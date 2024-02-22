2b34e33a1f71500e94602011de9fef9582812dd7
aniket-bhadra <aniketbhadra98320@gmail.com>
2024-02-22 11:03:25 +0530

    few style issues fixed & sound animation added

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let mySong = document.getElementById("mySong");
 let icon = document.getElementById("icon");
 let text = document.getElementById("text");
+let mainHeading = document.getElementById("main-heading");
+
+// console.log(mainHeading);
 
 icon.onclick = function () {
   if (mySong.paused) {
     mySong.play();
     icon.src = "assets/pause2.png";
     text.innerText = "Freeze the Beats";
+    mainHeading.classList.add("active");
   } else {
     mySong.pause();
     icon.src = "assets/play2.png";
     text.innerText = "Hit Play Now";
+    mainHeading.classList.remove("active");
   }
 };
